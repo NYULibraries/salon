82c32defa4f14dd35cc04fc9a37c323175c876b5
hab278 <hab278@nyu.edu>
2017-03-03 14:53:25 -0500

    use sockets

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 # set path to app that will be used to configure unicorn,
 # note the trailing slash in this example
-@dir = "./"
+@dir = "#{File.expand_path(File.dirname(__FILE__))}/"
 
 worker_processes 2
 working_directory @dir
@@ -9,8 +9,7 @@ timeout 30
 
 # Specify path to socket unicorn listens to,
 # we will use this in our nginx.conf later
-# listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
-listen 3000
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
 
 # Set process id path
 pid "#{@dir}tmp/pids/unicorn.pid"
