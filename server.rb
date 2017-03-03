bf4a2e4a25d6247386753b9a041984669b3eca03
hab278 <hab278@nyu.edu>
2017-03-03 13:24:12 -0500

    init commit

---
+++ server.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+require 'redis-sinatra'
+
+ENV['redis_host'] = 'localhost'
+ENV['redis_port'] = '6379'
+
+@@redis = Sinatra::Cache::RedisStore.new("#{ENV['redis_host']}:#{ENV['redis_port']}")
+@@redis.write("nyu", "https://www.nyu.edu")
+
+class Permalinks < Sinatra::Base
+  set :cache, @@redis
+
+  get '/:identifier' do
+    redirect_url = settings.cache.fetch("#{params['identifier']}")
+    redirect to(redirect_url) if redirect_url
+    status 400
+  end
+
+  not_found do
+    status 404
+  end
+
+  run! if app_file == $0
+end
