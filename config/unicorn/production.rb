0bbabf8197e626abe2a699cab35785de6a6cdcd2
Barnaby Alter <barnaby.alter@nyu.edu>
2017-04-17 13:59:13 -0400

    Docker working! Made sure to move the default localhost conf from the nginx image and dont start unicorn in detached mode

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -8,11 +8,11 @@ working_directory @dir
 timeout 30
 
 # Specify path to socket unicorn listens to,
-# we will use this in our nginx.conf later
-listen "#{@dir}tmp/sockets/unicorn.sock", backlog: 64
+# this location because its a permissions thing
+listen "/var/run/unicorn.sock", backlog: 64
 
 # Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "/app/tmp/pids/unicorn.pid"
 
 # Set log file paths
 stderr_path "#{@dir}log/unicorn.stderr.log"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Removing existing unicorn pid..."
-rm -rf tmp/unicorn.pid
+rm -rf tmp/pids/unicorn.pid
 
 echo "Running unicorn..."
-bundle exec unicorn -c config/unicorn/production.rb -p 8080 -E production -D
+bundle exec unicorn -c config/unicorn/production.rb -p 8080 -E production
