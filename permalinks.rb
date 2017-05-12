d3e03f6dd1da4f3d75659c44e421679562e4f511
Barnaby Alter <barnaby.alter@nyu.edu>
2017-05-12 14:11:05 -0400

    Add a 400 error as well

--- permalinks.rb
+++ permalinks.rb
@@ -13,4 +13,8 @@ class Permalinks < Sinatra::Base
   not_found do
     send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), { status: 404 })
   end
+
+  bad_request do
+    send_file(File.join(File.dirname(__FILE__), 'public', '400.html'), { status: 400 })
+  end
 end
