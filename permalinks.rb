0931932776d2f99f7326c91b66195e6105423fb6
hab278 <hab278@nyu.edu>
2017-05-12 14:39:52 -0400

    Add post json endpoint to set values

--- permalinks.rb
+++ permalinks.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'redis-store'
+require 'json'
 
 class Permalinks < Sinatra::Base
   set :cache, Redis::Store::Factory.create("#{ENV.fetch('REDIS_ADDRESS','localhost:6379')}", {marshalling: false})
@@ -13,6 +14,14 @@ class Permalinks < Sinatra::Base
     redirect_url = settings.cache.get("#{params['identifier']}")
     redirect to(redirect_url) if redirect_url
     status 400
+  end
+
+  post '/' do
+    parsed_json = JSON.parse(request.body.read)
+    parsed_json.keys.each do |key|
+      settings.cache.set(key, parsed_json[key])
+    end
+    status 200
   end
 
   not_found do
