2e24f096fb053799ccbf8501f68e003e4773e91c
Barnaby Alter <barnaby.alter@nyu.edu>
2017-05-12 13:54:32 -0400

    Added 404

--- permalinks.rb
+++ permalinks.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require 'redis-store'
 
 class Permalinks < Sinatra::Base
-  set :cache, Redis::Store::Factory.create("#{ENV.fetch('REDIS_ADDRESS','localhost:6379')}", {marshalling: false})
+  set :cache, Redis::Store::Factory.create("#{ENV.fetch('REDIS_ADDRESS','localhost:6379')}", { marshalling: false })
 
   get '/:identifier' do
     redirect_url = settings.cache.get("#{params['identifier']}")
@@ -11,6 +11,6 @@ class Permalinks < Sinatra::Base
   end
 
   not_found do
-    status 404
+    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), { status: 404 })
   end
 end
