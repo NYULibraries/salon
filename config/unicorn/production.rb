aa07e1e7900c0afd9ebf712fda7ff64e6812c83d
Barnaby Alter <barnaby.alter@nyu.edu>
2017-05-15 17:11:53 -0400

    Refining unicorn settings

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,18 +2,30 @@
 # note the trailing slash in this example
 @dir = "#{File.expand_path(File.dirname(__FILE__))}/../../"
 
+# Let X be your average memory usage, let B be your box's memory, and let C be your workers.
+# C = (B/X).floor # e.g. (512MB/200MB).floor = 2 workers
 worker_processes 2
 working_directory @dir
 
-timeout 30
+timeout 15
+preload_app true
 
 # Specify path to socket unicorn listens to,
 # this location because its a permissions thing
-listen "/var/run/unicorn.sock", backlog: 64
+# listen "/var/run/unicorn-#{ENV['APP_NAME']}.sock", backlog: 64
+listen (ENV['UNICORN_PORT'] || 8080)
 
 # Set process id path
 pid "#{@dir}tmp/pids/unicorn.pid"
 
 # Set log file paths
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+# stderr_path "#{@dir}log/unicorn.stderr.log"
+# stdout_path "#{@dir}log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+
+end
+
+after_fork do |server, worker|
+
+end
