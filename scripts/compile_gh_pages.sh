e639a4d0b2da73e4fffb009f8a0478b2ddf6b104
Eric Griffis <eric.griffis@nyu.edu>
2017-06-02 16:34:37 -0400

    Fix script

--- scripts/compile_gh_pages.sh
+++ scripts/compile_gh_pages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git checkout gh-pages
 bundle exec ruby ./scripts/swagger_to_json.rb
+mv swagger.json swagger_new.json
+git checkout gh-pages
+mv swagger_new.json swagger.json
 git add swagger.json
 git commit -m "Update swagger.json"
 git push
