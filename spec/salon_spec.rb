86979030d1389ad8a2e75d55f6c55497e1a64602
Eric Griffis <eric.griffis@nyu.edu>
2017-06-05 16:26:23 -0400

    Refactor tests

--- spec/salon_spec.rb
+++ spec/salon_spec.rb
@@ -1,33 +1,34 @@
 require_relative 'spec_helper'
 
 describe 'Salon' do
-  let(:example_site) { 'https://www.example.com/' }
-  before(:each) do
-    cache = double('cache', :get => nil)
-    allow(cache).to receive(:get).with('good_identifier') { example_site }
-    allow(Salon.settings).to receive(:cache) {cache}
-    get identifier
+  subject { last_response }
+
+  let(:cache){ double('cache', get: nil) }
+  before { allow(Salon.settings).to receive(:cache).and_return cache }
+
+  describe "/" do
+    before { get "/" }
+    it { is_expected.to be_not_found }
   end
 
-  context 'if no identifier is passed in' do
-    let(:identifier) {'/'}
-    it 'it should show documentation' do
-      expect(last_response).to be_not_found
+  describe "/:identifier" do
+    let(:example_site) { 'https://www.example.com/' }
+    before do
+      allow(cache).to receive(:get).with('good_identifier').and_return example_site
+      get "/#{identifier}"
     end
-  end
-  context 'if an identifier is passed in' do
-    context 'and the identifier is a key in the cache' do
-      let(:identifier) {'/good_identifier'}
-      it 'should be a redirect to the cache value' do
-        expect(last_response).to be_redirect
-        follow_redirect!
-        expect(last_request.url).to eql example_site
+    context 'if an identifier is passed in' do
+      context 'and the identifier is a key in the cache' do
+        let(:identifier) {'good_identifier'}
+        it 'should be a redirect to the cache value' do
+          expect(last_response).to be_redirect
+          follow_redirect!
+          expect(last_request.url).to eql example_site
+        end
       end
-    end
-    context 'and the identifier is not a key in the cache' do
-      let(:identifier) {'/bad_identifier'}
-      it 'should be a bad request' do
-        expect(last_response).to be_bad_request
+      context 'and the identifier is not a key in the cache' do
+        let(:identifier) {'bad_identifier'}
+        it { is_expected.to be_bad_request }
       end
     end
   end
