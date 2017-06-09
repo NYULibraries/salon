b01ba0613377b306f6bf9283f645d94c323b0698
Eric Griffis <eric.griffis@nyu.edu>
2017-06-09 13:59:03 -0400

    Remove vestigial script

--- scripts/compile_gh_pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bundle exec ruby ./scripts/swagger_to_json.rb
-mv swagger.json swagger_new.json
-git checkout gh-pages
-mv swagger_new.json swagger.json
-git add swagger.json
-git commit -m "Update swagger.json"
-git push
