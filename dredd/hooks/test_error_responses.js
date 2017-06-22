17a6908e4e8a39b89235b3fdbedceaec76a4c349
Barnaby Alter <barnaby.alter@nyu.edu>
2017-06-22 16:31:09 -0400

    Breaking Dredd tests; begin process of adding Oauth2 authentication to dredd tests

--- dredd/hooks/test_error_responses.js
+++ dredd/hooks/test_error_responses.js
@@ -1,4 +1,25 @@
 var hooks = require('hooks');
+var Client = require('node-rest-client').Client;
+var stash = {};
+
+hooks.beforeAll(function (transation) {
+  var client = new Client();
+  var url = "https://dev.login.library.nyu.edu/oauth/token";
+  var parameters = {
+    data: {
+      "grant_type": "client_credentials",
+      "client_id": "id",
+      "client_secret": "secreet",
+      "scope": "admin"
+    }
+  };
+  client.post(url, args, function (data, response) {
+    // parsed response body as js object
+    console.log(data);
+    // raw response
+    console.log(response);
+  });
+});
 
 hooks.beforeEach(function (transaction) {
   // don't run GET /{id} tests
@@ -8,7 +29,7 @@ hooks.beforeEach(function (transaction) {
   }
   // add auth headers unless testing 401
   if (transaction.expected.statusCode !== '401') {
-    transaction.request.headers.Auth = 'test_auth_key';
+    transaction.request.headers['Authorization'] = "Bearer " + stash['token']
   }
   // replace with bad JSON for 400
   if (transaction.expected.statusCode === '400') {
