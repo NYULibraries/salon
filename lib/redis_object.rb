82555d576d58ac0dbedba43c41cc0b1265f82c1f
Eric Griffis <eric.griffis@nyu.edu>
2017-09-06 16:28:34 -0400

    Update redis reference in helper

--- lib/redis_object.rb
+++ lib/redis_object.rb
@@ -3,6 +3,10 @@ class RedisObject
     @connection ||= Redis::Store::Factory.create("#{ENV.fetch('REDIS_HOST','localhost:6379')}", { marshalling: false })
   end
 
+  def self.exists?(id)
+    !!redis.get(id)
+  end
+
   def redis
     self.class.redis
   end
