816bc5ca901016ce148f29367e8aef36623eebd1
Eric Griffis <eric.griffis@nyu.edu>
2018-04-09 13:03:24 -0400

    Remove comments

--- spec/request/resource_spec.rb
+++ spec/request/resource_spec.rb
@@ -69,23 +69,4 @@ RSpec.describe "Resource management", type: :request do
       end
     end
   end
-
-
-  # it "creates a Widget and redirects to the Widget's page" do
-  #   get "/widgets/new"
-  #   expect(response).to render_template(:new)
-  #
-  #   post "/widgets", :widget => {:name => "My Widget"}
-  #
-  #   expect(response).to redirect_to(assigns(:widget))
-  #   follow_redirect!
-  #
-  #   expect(response).to render_template(:show)
-  #   expect(response.body).to include("Widget was successfully created.")
-  # end
-  #
-  # it "does not render a different template" do
-  #   get "/widgets/new"
-  #   expect(response).to_not render_template(:show)
-  # end
 end
