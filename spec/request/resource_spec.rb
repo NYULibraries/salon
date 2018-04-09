9d9bee059c169970ce3a67162326ec40e6903746
Eric Griffis <eric.griffis@nyu.edu>
2018-04-09 13:03:06 -0400

    Request spec - test various create link scenarios

--- spec/request/resource_spec.rb
+++ spec/request/resource_spec.rb
@@ -35,16 +35,37 @@ RSpec.describe "Resource management", type: :request do
 
       its(:status) { is_expected.to eq 401 }
       it "should not create new link" do
-        expect(PersistentLink.new(id: "abc").get_url).to eq nil
+        expect(RedisObject.redis.keys).to be_empty
       end
     end
 
     context "with authorization" do
       before { post "/", link_json, json_headers.merge(auth_headers) }
 
-      its(:status) { is_expected.to eq 201 }
-      it "should create new link" do
-        expect(PersistentLink.new(id: "abc").get_url).to eq "http://example.com"
+      context "with valid, full link" do
+        its(:status) { is_expected.to eq 201 }
+        it "should create new link" do
+          expect(PersistentLink.new(id: "abc").get_url).to eq "http://example.com"
+        end
+      end
+
+      context "with valid link with only URL" do
+        let(:link_hash){ {url: "http://example.com"} }
+
+        its(:status) { is_expected.to eq 201 }
+        it "should create new link" do
+          id = JSON.parse(subject.body)["id"]
+          expect(PersistentLink.new(id: id).get_url).to eq "http://example.com"
+        end
+      end
+
+      context "with invalid link with only ID" do
+        let(:link_hash){ {id: "abc"} }
+
+        its(:status) { is_expected.to eq 422 }
+        it "should not create new link" do
+          expect(RedisObject.redis.keys).to be_empty
+        end
       end
     end
   end
