e0c5dca58f318de4c12dc9fd964fa472e2a97fba
BAlter <barnaby.alter@nyu.edu>
2018-04-09 12:54:59 -0400

    Modularize views into partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,13 @@ require_relative '../lib/persistent_link_collection'
 class ApplicationController < Sinatra::Base
   enable :sessions
 
-
-  set :root, File.expand_path('../..', __FILE__)
-  set :public_folder, File.expand_path('../../../public', __FILE__)
-
   before do
     next unless request.post?
     content_type :json
   end
 
   not_found do
-    send_file(File.join(settings.public_folder, '40x.html'), { status: 404 })
+    erb :not_found
   end
 
 end
